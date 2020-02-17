104da1381fe30c94fe5e41bed43368d8b36be5fe
gbruzas <gediminas.bruzas@bazaarvoice.com>
2020-02-17 14:14:50 +0200

    EscateTest case fixes

--- Escape Tool/EscapeTool.test.js
+++ Escape Tool/EscapeTool.test.js
@@ -7,12 +7,13 @@ describe("EscapeTool", function() {
   let vars;
   beforeEach(async function() {
     driver = await new Builder().forBrowser("chrome").build();
+    await driver.manage().setTimeouts({ implicit: 15000 });
     vars = {};
   });
   afterEach(async function() {
     await driver.quit();
   });
-  it("EscapeTool", async function() {
+  it("Escapes from: ども ありがと ｍｒ。 ろぼと ", async function() {
     await driver.get(
       "https://cstools.flynn-qa-us-east-1.nexus.bazaarvoice.com/"
     );
@@ -25,13 +26,20 @@ describe("EscapeTool", function() {
     await driver.findElement(By.id("escape")).click();
     assert(
       (await driver.findElement(By.id("outputArea")).getText()) ==
-        "\\\\u3069\\\\u3082 \\\\u3042\\\\u308A\\\\u304C\\\\u3068 \\\\uFF4D\\\\uFF52\\\\u3002 \\\\u308D\\\\u307C\\\\u3068"
+        "\\u3069\\u3082 \\u3042\\u308A\\u304C\\u3068 \\uFF4D\\uFF52\\u3002 \\u308D\\u307C\\u3068"
     );
+  });
+  it("Escapes from: 7365637265742068657861646563696d616c20656e636f64696e67", async function() {
+    await driver.get(
+      "https://cstools.flynn-qa-us-east-1.nexus.bazaarvoice.com/"
+    );
+    await driver.findElement(By.css(".Burger")).click();
+    await driver.findElement(By.linkText("Escape Tool")).click();
+    await driver.findElement(By.id("inputArea")).click();
     await driver
       .findElement(By.id("inputArea"))
       .sendKeys("7365637265742068657861646563696d616c20656e636f64696e67");
     await driver.findElement(By.id("hex2ascii")).click();
-    await driver.findElement(By.id("outputArea")).click();
     assert(
       (await driver.findElement(By.id("outputArea")).getText()) ==
         "secret hexadecimal encoding"
