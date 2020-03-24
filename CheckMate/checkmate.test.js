0c3f81716ce711fe7875423238c5eccfb635b617
gbruzas <gediminas.bruzas@bazaarvoice.com>
2020-03-24 13:19:42 +0200

    updates

--- CheckMate/checkmate.test.js
+++ CheckMate/checkmate.test.js
@@ -1,4 +1,4 @@
-const chrome = require("selenium-webdriver/chrome");
+// const chrome = require("selenium-webdriver/chrome");
 const { Builder, By, until } = require("selenium-webdriver");
 const assert = require("assert");
 
--- UASGenerator/UASGenrator.test.js
+++ UASGenerator/UASGenrator.test.js
@@ -2,44 +2,46 @@ const { Builder, By, until, Key } = require("selenium-webdriver");
 const assert = require("assert");
 const addContext = require("mochawesome/addContext");
 
+const driver = new Builder().forBrowser("chrome").build();
+
 describe("UAS Generator", function() {
-  this.timeout(30000);
-  let driver;
-  beforeEach(async function() {
-    driver = await new Builder().forBrowser("chrome").build();
-    await driver.manage().setTimeouts({ implicit: 15000 });
-  });
-  afterEach(async function() {});
-  it("generates and Validates User Auth String", async function() {
-    await driver.get(
-      "https://cstools.flynn-qa-us-east-1.nexus.bazaarvoice.com/"
-    );
-    await driver.findElement(By.linkText("UAS Generator")).click();
-    await driver.findElement(By.css("input")).click();
-    await driver.findElement(By.css("input")).sendKeys("TestCustomer");
-    await driver.findElement(By.css(".Button")).click();
-    await driver.findElement(By.xpath("//input[@value='']")).click();
-    await driver.findElement(By.xpath("//div[5]/div[7]")).click();
-    await driver.findElement(By.id("userid")).click();
-    await driver.findElement(By.id("userid")).sendKeys("TestID");
-    await driver.findElement(By.id("email")).click();
-    await driver.findElement(By.id("email")).sendKeys("email@adress.com");
-    await driver.findElement(By.id("username")).click();
-    await driver.findElement(By.id("username")).sendKeys("NameUser");
-    await driver.findElement(By.id("maxage")).click();
-    await driver.findElement(By.id("maxage")).sendKeys("45");
-    await driver.findElement(By.css(".Button:nth-child(3)")).click();
-    await driver.findElement(By.css(".FormTextArea")).click();
-    const uas = await driver
-      .findElement(By.css(".TextBlock:nth-child(2) > code"))
-      .getText();
-    await driver.findElement(By.css(".FormTextArea")).sendKeys(uas);
-    await driver.findElement(By.css(".normal")).click();
-    assert(
-      (await driver.findElement(By.css(".success > .\\_Rfxe_")).getText()) ==
-        "success!"
-    );
-    addContext(this, "UAS used in this test case:");
-    addContext(this, uas);
+  it("should open CS tools page and", async function() {
+    driver.get("https://cstools.flynn-qa-us-east-1.nexus.bazaarvoice.com/");
+    const title = await driver.findElement(By.xpath("//span[2]")).getText();
+    assert.equal(title, "Home");
   });
 });
+
+//   it("generates and Validates User Auth String", async function() {
+//     await driver.get(
+//       "https://cstools.flynn-qa-us-east-1.nexus.bazaarvoice.com/"
+//     );
+//     await driver.findElement(By.linkText("UAS Generator")).click();
+//     await driver.findElement(By.css("input")).click();
+//     await driver.findElement(By.css("input")).sendKeys("TestCustomer");
+//     await driver.findElement(By.css(".Button")).click();
+//     await driver.findElement(By.xpath("//input[@value='']")).click();
+//     await driver.findElement(By.xpath("//div[5]/div[7]")).click();
+//     await driver.findElement(By.id("userid")).click();
+//     await driver.findElement(By.id("userid")).sendKeys("TestID");
+//     await driver.findElement(By.id("email")).click();
+//     await driver.findElement(By.id("email")).sendKeys("email@adress.com");
+//     await driver.findElement(By.id("username")).click();
+//     await driver.findElement(By.id("username")).sendKeys("NameUser");
+//     await driver.findElement(By.id("maxage")).click();
+//     await driver.findElement(By.id("maxage")).sendKeys("45");
+//     await driver.findElement(By.css(".Button:nth-child(3)")).click();
+//     await driver.findElement(By.css(".FormTextArea")).click();
+//     const uas = await driver
+//       .findElement(By.css(".TextBlock:nth-child(2) > code"))
+//       .getText();
+//     await driver.findElement(By.css(".FormTextArea")).sendKeys(uas);
+//     await driver.findElement(By.css(".normal")).click();
+//     assert(
+//       (await driver.findElement(By.css(".success > .\\_Rfxe_")).getText()) ==
+//         "success!"
+//     );
+//     addContext(this, "UAS used in this test case:");
+//     addContext(this, uas);
+//   });
+// });
