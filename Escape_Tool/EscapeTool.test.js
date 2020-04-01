9ab91a4d964eb9492cd85c962f7f042c650589a7
gbruzas <gediminas.bruzas@bazaarvoice.com>
2020-04-01 22:49:31 +0300

    updates

--- Escape_Tool/EscapeTool.test.js
+++ Escape_Tool/EscapeTool.test.js
@@ -2,7 +2,6 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
 const assert = require("assert");
 const fs = require("fs");
 const addContext = require("mochawesome/addContext");
-
 const driver = new Builder().forBrowser("chrome").build();
 
 describe("EscapeTool", function() {
@@ -27,27 +26,40 @@ describe("EscapeTool", function() {
         "\\u3069\\u3082 \\u3042\\u308A\\u304C\\u3068 \\uFF4D\\uFF52\\u3002 \\u308D\\u307C\\u3068"
     );
     await driver.takeScreenshot().then(function(data) {
-      fs.writeFileSync("img.png", data, "base64");
+      fs.writeFileSync("escape.png", data, "base64");
     });
     addContext(
       this,
-      "/Users/gediminas.bruzas/Documents/TestCases/Escape_Tool/img.png"
+      "/Users/gediminas.bruzas/Documents/TestCases/Escape_Tool/escape.png"
     );
   });
-  it("Escapes from: 7365637265742068657861646563696d616c20656e636f64696e67", async function() {
+  it("Escapes from: 7365637265742068657861646563696d616c20656e636f64696e67. It opens CS tools.", async function() {
     await driver.get(
       "https://cstools.flynn-qa-us-east-1.nexus.bazaarvoice.com/"
     );
-    await driver.findElement(By.css(".Burger")).click();
+  });
+  it("opens Escape tool", async function() {
     await driver.findElement(By.linkText("Escape Tool")).click();
-    await driver.findElement(By.id("inputArea")).click();
+  });
+  it("enter keys: 7365637265742068657861646563696d616c20656e636f64696e67", async function() {
     await driver
       .findElement(By.id("inputArea"))
       .sendKeys("7365637265742068657861646563696d616c20656e636f64696e67");
+  });
+  it("Clicks teh button: hex2ascii", async function() {
     await driver.findElement(By.id("hex2ascii")).click();
+  });
+  it("confitms the output is equal to - secret hexadecimal encoding", async function() {
     assert(
       (await driver.findElement(By.id("outputArea")).getText()) ==
         "secret hexadecimal encoding"
+    );
+    await driver.takeScreenshot().then(function(data) {
+      fs.writeFileSync("hex2ascii.png", data, "base64");
+    });
+    addContext(
+      this,
+      "/Users/gediminas.bruzas/Documents/TestCases/Escape_Tool/hex2ascii.png"
     );
   });
 });
