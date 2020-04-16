aa7c8055f1e777ca71ad09ddea463e32e883a07c
gbruzas <gediminas.bruzas@bazaarvoice.com>
2020-04-16 13:23:42 +0300

    small uodates

--- CheckMate/checkmate.test.js
+++ CheckMate/checkmate.test.js
@@ -13,55 +13,55 @@ const driver = new Builder()
   //     .headless().windowSize(screen))
   .build();
 
-describe("Checkmate Test", function() {
-  it("should navigate to Scraper page and confirm that the page title is 'Scraper'", async function() {
+describe("Checkmate Test", function () {
+  it("should navigate to Scraper page and confirm that the page title is 'Scraper'", async function () {
     driver.get(
       "https://checkmate-frontend.us-east-1a.bosun.prod.bazaarvoice.com/scraper"
     );
     const title = await driver.findElement(By.xpath("//span[2]")).getText();
     assert.equal(title, "Scraper");
   });
-  it("Should enter PDP URL", async function() {
+  it("Should enter PDP URL", async function () {
     await driver
       .findElement(By.css(".FormRow:nth-child(1) .FormTextInput"))
       .sendKeys(
         "https://www.styltom.co.uk/collections/all-products/products/stylpro-easy-on-the-eye"
       );
   });
-  it("Clicks Submit button", async function() {
+  it("Clicks Submit button", async function () {
     await driver.findElement(By.css(".Button")).click();
   });
-  it("Checks whether the PDP details has been returned. Waiting limit 30s.", async function() {
+  it("Checks whether the PDP details has been returned. Waiting limit 30s.", async function () {
     await driver.wait(
       until.elementLocated(By.css("div:nth-child(1) > h1")),
       30000
     );
   });
-  it("checks whether the Page info table is present", async function() {
+  it("checks whether the Page info table is present", async function () {
     const pageInfo = await driver.findElements(
       By.css(".\\_Rfx5_ > div > div:nth-child(1)")
     );
     assert(pageInfo.length);
   });
-  it("checks whether $BVConfigure table is present", async function() {
+  it("checks whether $BVConfigure table is present", async function () {
     const BVConfigure = await driver.findElements(
       By.css(".\\_Rfx5_ > div > div:nth-child(2)")
     );
     assert(BVConfigure.length);
   });
-  it("checks whether BVRRContainer table is present", async function() {
+  it("checks whether BVRRContainer table is present", async function () {
     const BVRRContainer = await driver.findElements(
       By.css(".\\_Rfx5_ > div > div:nth-child(3)")
     );
     assert(BVRRContainer.length);
   });
-  it("checks whether SEO Sting table is present", async function() {
+  it("checks whether SEO Sting table is present", async function () {
     const seoString = await driver.findElements(
       By.css(".\\_Rfx5_ > div > div:nth-child(3)")
     );
     assert(seoString.length);
   });
-  it("checks whether BV Script table is present", async function() {
+  it("checks whether BV Script table is present", async function () {
     const bvScript = await driver.findElements(
       By.css(".\\_Rfx5_ div:nth-child(5)")
     );
--- Escape_Tool/EscapeTool.test.js
+++ Escape_Tool/EscapeTool.test.js
@@ -4,62 +4,56 @@ const fs = require("fs");
 const addContext = require("mochawesome/addContext");
 const driver = new Builder().forBrowser("chrome").build();
 
-describe("EscapeTool", function() {
-  it("Goes to CS tools", async function() {
+describe("EscapeTool", function () {
+  it("Goes to CS tools", async function () {
     await driver.get(
       "https://cstools.flynn-qa-us-east-1.nexus.bazaarvoice.com/"
     );
   });
-  it("Selects Escape tool from the menu:", async function() {
+  it("Selects Escape tool from the menu:", async function () {
     await driver.findElement(By.css(".Burger")).click();
     await driver.findElement(By.linkText("Escape Tool")).click();
   });
-  it("Enter keys'ども ありがと ｍｒ。ろぼと'", async function() {
+  it("Enter keys'ども ありがと ｍｒ。ろぼと'", async function () {
     await driver
       .findElement(By.id("inputArea"))
       .sendKeys("ども ありがと ｍｒ。 ろぼと");
     await driver.findElement(By.id("escape")).click();
   });
-  it('verifies the output is equal: "\\u3069\\u3082 \\u3042\\u308A\\u304C\\u3068 \\uFF4D\\uFF52\\u3002 \\u308D\\u307C\\u3068"', async function() {
+  it('verifies the output is equal: "\\u3069\\u3082 \\u3042\\u308A\\u304C\\u3068 \\uFF4D\\uFF52\\u3002 \\u308D\\u307C\\u3068"', async function () {
     assert(
       (await driver.findElement(By.id("outputArea")).getText()) ==
         "\\u3069\\u3082 \\u3042\\u308A\\u304C\\u3068 \\uFF4D\\uFF52\\u3002 \\u308D\\u307C\\u3068"
     );
-    await driver.takeScreenshot().then(function(data) {
+    await driver.takeScreenshot().then(function (data) {
       fs.writeFileSync("escape.png", data, "base64");
     });
-    addContext(
-      this,
-      "/Users/gediminas.bruzas/Documents/TestCases/Escape_Tool/escape.png"
-    );
+    addContext(this, `${__dirname}/escape.png`);
   });
-  it("Escapes from: 7365637265742068657861646563696d616c20656e636f64696e67. It opens CS tools.", async function() {
+  it("Escapes from: 7365637265742068657861646563696d616c20656e636f64696e67. It opens CS tools.", async function () {
     await driver.get(
       "https://cstools.flynn-qa-us-east-1.nexus.bazaarvoice.com/"
     );
   });
-  it("opens Escape tool", async function() {
+  it("opens Escape tool", async function () {
     await driver.findElement(By.linkText("Escape Tool")).click();
   });
-  it("enter keys: 7365637265742068657861646563696d616c20656e636f64696e67", async function() {
+  it("enter keys: 7365637265742068657861646563696d616c20656e636f64696e67", async function () {
     await driver
       .findElement(By.id("inputArea"))
       .sendKeys("7365637265742068657861646563696d616c20656e636f64696e67");
   });
-  it("Clicks teh button: hex2ascii", async function() {
+  it("Clicks teh button: hex2ascii", async function () {
     await driver.findElement(By.id("hex2ascii")).click();
   });
-  it("confitms the output is equal to - secret hexadecimal encoding", async function() {
+  it("confitms the output is equal to - secret hexadecimal encoding", async function () {
     assert(
       (await driver.findElement(By.id("outputArea")).getText()) ==
         "secret hexadecimal encoding"
     );
-    await driver.takeScreenshot().then(function(data) {
+    await driver.takeScreenshot().then(function (data) {
       fs.writeFileSync("hex2ascii.png", data, "base64");
     });
-    addContext(
-      this,
-      "/Users/gediminas.bruzas/Documents/TestCases/Escape_Tool/hex2ascii.png"
-    );
+    addContext(this, `${__dirname}/hex2ascii.png`);
   });
 });
